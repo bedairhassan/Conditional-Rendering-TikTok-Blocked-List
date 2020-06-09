4a02dfb149e460706c28c0f036d3d378f65dd9da
hassan bedair <55901623+bedairhassan@users.noreply.github.com>
2020-06-09 20:13:04 +0200

    6. Creating Block, Unblocked Buttons,

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,49 +1,81 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
 
-  const [data,dataSet]=useState(
+  const [data, dataSet] = useState(
     [
-      {id:1,name:`Hassan`,isBlocked:false},
-      {id:2,name:`Aly`,isBlocked:false},
-      {id:3,name:`Mohammed`,isBlocked:false},
-      {id:4,name:`Mostafa`,isBlocked:true},
+      { id: 1, name: `Hassan`, isBlocked: false },
+      { id: 2, name: `Aly`, isBlocked: false },
+      { id: 3, name: `Mohammed`, isBlocked: false },
+      { id: 4, name: `Mostafa`, isBlocked: true },
     ])
 
   return (<React.Fragment>
-    <Table data={data} headers={[`id`,`name`,`isBlocked`]}/>
+
+
+
+
+
+    <Table data={data} headers={[`id`, `name`, `isBlocked`]} />
   </React.Fragment>)
 }
+
+const Button = ({ onClick, name, className }) =>
+  <button
+    className={className}
+    onClick={onClick}>{name}</button>
 
 const Table = ({ data, headers }) => {
 
   return (
     <React.Fragment>
       This is Table.
-      <button type="button" class="btn btn-primary">Primary</button>
 
-      <Headers headers={headers}/>
+      <Headers headers={headers} />
 
-      <Body data={data}/>
+      <Body data={data} />
     </React.Fragment>
   )
 }
 
-const Body = ({data})=>
-  data.map(({id,name,isBlocked})=><tr>
-    <td>{id}</td>
-    <td>{name}</td>
-    <td>{isBlocked ? `yes`:`no`}</td>
-  </tr>)
+const Body = ({ data }) =>
+  data.map(({ id, name, isBlocked }) => {
+
+    // const idd = `id is ${id}`
+
+    const BlockElement = !isBlocked ?
+      <Button
+        onClick={() => alert(`block`)}
+        name={`Block`}
+        className={"btn btn-danger"}
+      /> :
+      <Button
+        onClick={() => alert(`unblock`)}
+        name={`Unblock`}
+        className={"btn btn-secondary"}
+      />
 
 
-const Headers = ({headers})=><thead>
-{headers.map(header=><Header header={header}/>)}
+    return (
+
+      <tr key={id}>
+        {/* <td>{idd}</td> */}
+        <td>{name}</td>
+        <td>{isBlocked ? `yes` : `no`}</td>
+        <td>{BlockElement}</td>
+      </tr>
+    )
+
+  })
+
+
+const Headers = ({ headers }) => <thead>
+  {headers.map(header => <Header header={header} />)}
 </thead>
 
-const Header = ({header})=>
-<th>{header}</th>
+const Header = ({ header }) =>
+  <th>{header}</th>
 
 export default App;
