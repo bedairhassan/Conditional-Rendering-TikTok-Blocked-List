247763f0d83b1dd91fe60c1f08bb6b2fa0104541
hassan bedair <55901623+bedairhassan@users.noreply.github.com>
2020-06-09 19:59:14 +0200

    4. Creating A Table

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [data,dataSet]=useState(
+    [
+      {id:1,name:`Hassan`,isBlocked:false},
+      {id:2,name:`Aly`,isBlocked:false},
+      {id:3,name:`Mohammed`,isBlocked:false},
+      {id:4,name:`Mostafa`,isBlocked:true},
+    ])
+
+  return (<React.Fragment>
+    <Table data={data} headers={[`id`,`name`,`isBlocked`]}/>
+  </React.Fragment>)
+}
+
+const Table = ({ data, headers }) => {
+
   return (
     <React.Fragment>
-      Hello World
+      This is Table.
+
+      <Headers headers={headers}/>
+
+      <Body data={data}/>
     </React.Fragment>
-  );
+  )
 }
+
+const Body = ({data})=>
+  data.map(({id,name,isBlocked})=><tr>
+    <td>{id}</td>
+    <td>{name}</td>
+    <td>{isBlocked ? `yes`:`no`}</td>
+  </tr>)
+
+
+const Headers = ({headers})=><thead>
+{headers.map(header=><Header header={header}/>)}
+</thead>
+
+const Header = ({header})=>
+<th>{header}</th>
 
 export default App;
